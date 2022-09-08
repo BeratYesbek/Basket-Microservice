991c608a047ddb3150056c17ce7562486cf07895
Feanor <beratyesbek@gmail.com>
2022-09-08 21:26:23 +0300

    Service registration and dependency resolvers have been created.

---
+++ WebAPI/Extensions/DependencyResolver.cs
@@ -0,0 +1,16 @@
+﻿using WebAPI.Repositories.Abstracts;
+using WebAPI.Repositories.Concretes;
+
+namespace WebAPI.Extensions
+{
+    public static class DependencyResolver
+    {
+        public static void LoadResolver(this IServiceCollection service)
+        {
+            service.AddSingleton<IProductRepository, ProductRepository>();
+            service.AddSingleton<ICategoryRepository, CategoryRepository>();
+            service.AddSingleton<IProductImageRepository, ProductImageRepository>();
+            service.AddSingleton<IShoppingCartRepository, ShoppingCartRepository>();
+        }
+    }
+}
---
+++ WebAPI/Extensions/ServiceRegistrations.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
+namespace WebAPI.Extensions
+{
+    public static class ServiceRegistrations
+    {
+        public static void ServiceRegister(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<BasketDbContext>(opt => opt.UseSqlServer(
+                configuration.GetConnectionString("BasketConnectionString")
+                ));
+
+        }
+    }
+}
